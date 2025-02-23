37c43207f70c96cf42112b0503154822b902ce3b
Manigandan13 <jaiselvi13072005@gmail.com>
2025-02-23 23:33:17 +0530

    finished

---
+++ src/main/java/com/crypto/service/PaymentDetailsService.java
@@ -0,0 +1,18 @@
+package com.crypto.service;
+
+import com.crypto.model.PaymentDetails;
+import com.crypto.model.User;
+import jakarta.persistence.OneToOne;
+
+public interface PaymentDetailsService {
+    public PaymentDetails addPaymentDetails( String accountNumber,
+                                             String accountHolderName,
+                                             String ifsc,
+                                             String bankName,
+                                             User user
+    );
+
+    public PaymentDetails getUsersPaymentDetails(User user);
+
+
+}
